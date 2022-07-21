ef9c123b26e3fb4e2a4131576c4895989fd265ad
sean.xiao <zy05530@gmail.com>
2022-07-21 17:01:39 +1200

    edit user

--- app/routes/admin/users/$userId.tsx
+++ app/routes/admin/users/$userId.tsx
@@ -1,12 +1,105 @@
-import { Button } from "@chakra-ui/react";
-import { Link } from "@remix-run/react";
+import {
+  Button,
+  Center,
+  Checkbox,
+  Container,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  Spacer,
+} from "@chakra-ui/react";
+import type { User } from "@prisma/client";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Form, Link, useLoaderData, useTransition } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import { requireAdmin } from "~/utils/session.server";
+import bcrypt from "bcryptjs";
+import { useState } from "react";
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  await requireAdmin(request);
+  const userId = params.userId!;
+  const user = await db.user.findFirst({ where: { id: userId } });
+
+  return user;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  await requireAdmin(request);
+  const userId = params.userId!;
+  const form = await request.formData();
+  const username = form.get("username")!.toString();
+  const password = form.get("password")?.toString();
+  const isAdmin = Boolean(form.get("isAdmin"));
+  let passwordHash;
+  if (password) {
+    passwordHash = await bcrypt.hash(password, 10);
+  }
+
+  await db.user.update({
+    data: password
+      ? {
+          username,
+          isAdmin,
+          passwordHash,
+        }
+      : {
+          username,
+          isAdmin,
+        },
+    where: { id: userId },
+  });
+
+  return redirect("/admin/users");
+};
 
 export default function EditUser() {
+  const { state } = useTransition();
+  const user = useLoaderData<User>();
+  const [isAdmin, setIsAdmin] = useState<boolean>(user.isAdmin);
+
   return (
-    <>
-      <Button colorScheme="blue" as={Link} to="/admin/users">
-        Back
-      </Button>
-    </>
+    <Center height="100vh" width="100vw">
+      <Container>
+        <Heading marginY={8}>Edit a user</Heading>
+        <Form method="post">
+          <FormControl isRequired>
+            <Input
+              name="username"
+              placeholder="Name"
+              marginY={4}
+              defaultValue={user.username}
+            />
+          </FormControl>
+          <Input name="password" placeholder="Password" marginY={4} />
+          <p>*Leave password blank if you don't want to change it.</p>
+          <Checkbox
+            value={String(isAdmin)}
+            isChecked={isAdmin}
+            name="isAdmin"
+            marginY={4}
+            onChange={(e) => setIsAdmin(e.target.checked)}
+          >
+            Is manager?
+          </Checkbox>
+
+          <Flex marginY={8}>
+            <Button colorScheme="blue" as={Link} to="/admin/users">
+              Back
+            </Button>
+            <Spacer />
+            <Button
+              colorScheme="teal"
+              type="submit"
+              disabled={state === "submitting"}
+            >
+              Update
+            </Button>
+          </Flex>
+        </Form>
+      </Container>
+    </Center>
   );
 }
